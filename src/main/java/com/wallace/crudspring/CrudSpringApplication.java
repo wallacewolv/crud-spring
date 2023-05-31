e4c73fd9df8512840923480959dc324e0387531a
wallace <wallace_wolv@hotmail.com>
2023-05-31 00:58:08 -0300

    feat: Listar Cursos (API HTTP GET)

--- src/main/java/com/wallace/crudspring/CrudSpringApplication.java
+++ src/main/java/com/wallace/crudspring/CrudSpringApplication.java
@@ -1,13 +1,31 @@
 package com.wallace.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.wallace.crudspring.model.Course;
+import com.wallace.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course course = new Course();
+			course.setName("Angular com Spring");
+			course.setCategory("front-end");
+
+			courseRepository.save(course);
+		};
 	}
 
 }
---
+++ src/main/java/com/wallace/crudspring/repository/CourseRepository.java
@@ -0,0 +1,13 @@
+package com.wallace.crudspring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wallace.crudspring.model.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+  
+
+}
