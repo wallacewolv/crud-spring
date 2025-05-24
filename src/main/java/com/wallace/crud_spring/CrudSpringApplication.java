44fbb39bc0142ea83aea420ec29ac9308002771f
Wallace W <wallace_wolv@hotmail.com>
2025-05-24 20:44:03 -0300

    initial commit

---
+++ src/main/java/com/wallace/crud_spring/CrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.wallace.crud_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+}
