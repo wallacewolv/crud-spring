4f67f939b83efb46ee31d0ff719fd918800b5c1e
Wallace W <wallace_wolv@hotmail.com>
2025-05-24 21:50:58 -0300

    feat: 15: Listar Cursos (API HTTP GET)

--- src/main/java/com/wallace/crud_spring/CrudSpringApplication.java
+++ src/main/java/com/wallace/crud_spring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.wallace.crud_spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.wallace.crud_spring.model.Course;
+import com.wallace.crud_spring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,16 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course course = new Course();
+			course.setName("Angular com Spring");
+			course.setCategory("front-end");
+
+			courseRepository.save(course);
+		};
+	}
 }
