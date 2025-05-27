e6c6c62b866f8da2f035e5f7f9509a0639961e65
Wallace W <wallace_wolv@hotmail.com>
2025-05-27 00:00:41 -0300

    feat: 21: Formulário: API Spring: Criar Curso (HTTP POST)

--- src/main/java/com/wallace/crud_spring/controller/CourseController.java
+++ src/main/java/com/wallace/crud_spring/controller/CourseController.java
@@ -2,9 +2,15 @@ package com.wallace.crud_spring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wallace.crud_spring.model.Course;
@@ -22,5 +28,14 @@ public class CourseController {
   @GetMapping
   public @ResponseBody List<Course> list() {
     return courseRepository.findAll();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Course create(@RequestBody Course course) {
+    return courseRepository.save(course);
+    
+    // return ResponseEntity.status(HttpStatus.CREATED)
+    //   .body(courseRepository.save(course));
   }
 }
