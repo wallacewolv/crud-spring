3dfc42ce18cf136acf964aa48044b14d8435ad5e
Wallace W <wallace_wolv@hotmail.com>
2025-05-27 17:08:22 -0300

    feat: 26: API Spring: Buscar por ID

--- src/main/java/com/wallace/crud_spring/controller/CourseController.java
+++ src/main/java/com/wallace/crud_spring/controller/CourseController.java
@@ -2,10 +2,12 @@ package com.wallace.crud_spring.controller;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,7 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/api/courses")
 @AllArgsConstructor
 public class CourseController {
-  
+
   private final CourseRepository courseRepository;
 
   @GetMapping
@@ -30,12 +32,19 @@ public class CourseController {
     return courseRepository.findAll();
   }
 
+  @GetMapping({ "/{id}" })
+  public ResponseEntity<Course> findById(@PathVariable Long id) {
+    return courseRepository.findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
+  }
+
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public Course create(@RequestBody Course course) {
     return courseRepository.save(course);
-    
+
     // return ResponseEntity.status(HttpStatus.CREATED)
-    //   .body(courseRepository.save(course));
+    // .body(courseRepository.save(course));
   }
 }
