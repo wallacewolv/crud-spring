d69d6836f59a4a5db9c18ace91df5dad7c5ba90d
Wallace W <wallace_wolv@hotmail.com>
2025-05-27 20:43:08 -0300

    feat: 30: Atualizar Curso: API Spring HTTP PUT

--- src/main/java/com/wallace/crud_spring/controller/CourseController.java
+++ src/main/java/com/wallace/crud_spring/controller/CourseController.java
@@ -2,13 +2,12 @@ package com.wallace.crud_spring.controller;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,10 +31,10 @@ public class CourseController {
     return courseRepository.findAll();
   }
 
-  @GetMapping({ "/{id}" })
+  @GetMapping("/{id}")
   public ResponseEntity<Course> findById(@PathVariable Long id) {
     return courseRepository.findById(id)
-        .map(record -> ResponseEntity.ok().body(record))
+        .map(recordFound -> ResponseEntity.ok().body(recordFound))
         .orElse(ResponseEntity.notFound().build());
   }
 
@@ -46,5 +45,18 @@ public class CourseController {
 
     // return ResponseEntity.status(HttpStatus.CREATED)
     // .body(courseRepository.save(course));
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+    return courseRepository.findById(id)
+        .map(recordFound -> {
+          recordFound.setName(course.getName());
+          recordFound.setCategory(course.getCategory());
+
+          Course updated = courseRepository.save(recordFound);
+          return ResponseEntity.ok().body(updated);
+        })
+        .orElse(ResponseEntity.notFound().build());
   }
 }
