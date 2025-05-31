76863c60ff725be1572a5fa2949a591618c0a4df
Wallace W <wallace_wolv@hotmail.com>
2025-05-31 19:38:51 -0300

    feat: 36: Remoção Lógica (Soft Delete)

--- src/main/java/com/wallace/crud_spring/model/Course.java
+++ src/main/java/com/wallace/crud_spring/model/Course.java
@@ -1,5 +1,7 @@
 package com.wallace.crud_spring.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -17,6 +19,8 @@ import lombok.Data;
 @Data
 @Entity
 // @Table(name = "courses")
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
 
   @Id
@@ -30,11 +34,15 @@ public class Course {
   @Column(length = 100, nullable = false)
   private String name;
   
-  @NotBlank
   @NotNull
   @Length(max = 10)
   @Pattern(regexp = "Back-end|Front-end")
   @Column(length = 10, nullable = false)
   private String category;
 
+  @NotNull
+  @Length(max = 10)
+  @Pattern(regexp = "Ativo|Inativo")
+  @Column(length = 10, nullable = false)
+  private String status = "Ativo";
 }
